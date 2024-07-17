bf02ccdaa744e839199efb45ad7dc941c9b43823
Anirban Dey <anirbandey@soufrabi.com>
2024-07-17 08:51:13 +0530

    Add fields exitCode, time, wallTime, memory
    to RunRceeResponse record

--- src/main/java/com/soufrabi/nextcode/server/ApiServer/editor/run/RunRceeResponse.java
+++ src/main/java/com/soufrabi/nextcode/server/ApiServer/editor/run/RunRceeResponse.java
@@ -3,6 +3,10 @@ package com.soufrabi.nextcode.server.ApiServer.editor.run;
 public record RunRceeResponse(
         String stdout,
         String stderr,
-        String status
+        String status,
+        String exitCode,
+        String time,
+        String wallTime,
+        String memory
 ) {
 }
